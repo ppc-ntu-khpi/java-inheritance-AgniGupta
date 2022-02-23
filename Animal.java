ffec8457881137dc1f110bf0b4363a232ad6c04b
AgniGupta <66411181+AgniGupta@users.noreply.github.com>
2022-02-23 17:48:10 +0200

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class Animal {
+
+    protected String name;
+
+    protected  int weight;
+
+    protected  int age;
+
+    protected  Object untitledField;
+
+    public Animal() {
+        name="generic animal"; 
+        weight=70;
+        age=12;
+    }
+
+    public void eat() {
+        System.out.println("Animal is eating...");
+    }
+
+    public void speak() {
+        System.out.println("Animal is speaking...");
+    }
+
+    @Override
+    public String toString() {
+        return "Animal:" + "\nName:\t" + name + "\nWeight:\t" + weight + " kg" + "\nAge:\t" + age ;
+    }
+
+}
